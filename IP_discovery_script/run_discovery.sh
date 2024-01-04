60b5368904a6aa8f83a5a06cd0b25b99ad91ba8c
AddyKwach <adriankwach@gmail.com>
2024-01-04 12:47:13 +0300

    Changes to shell script

--- IP_discovery_script/run_discovery.sh
+++ IP_discovery_script/run_discovery.sh
@@ -5,7 +5,7 @@
 # Ensure Python3 is available
 if command -v python3 &> /dev/null; then
 	    # Run the Python script
-	     python3 network_device_discovery.py
+	     python3 ip_discovery_script.py
 	else
              echo "Error: Python3 is not installed."
 	     exit 1
